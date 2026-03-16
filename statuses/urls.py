6c59cc3304ecba4ad2baba3b658d890f345c7717
Alex <klyaksaoff@gmail.com>
2026-03-16 17:31:37 +0300

    fix linter

--- statuses/urls.py
+++ statuses/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path('statuses/', views.status, name='statuses'),
     path('statuses/create/', views.status_create, name='status_create'),
-    path('statuses/<int:pk>/update/', views.update_status, name='status_update'),
-    path('statuses/<int:pk>/delete/', views.delete_status, name='status_delete'),
+    path('statuses/<int:pk>/update/', views.update_status,
+         name='status_update'),
+    path('statuses/<int:pk>/delete/', views.delete_status,
+         name='status_delete'),
 ]
 
